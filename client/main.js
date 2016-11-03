d0cd79021c3dd03d3c5a05e4c1794180725869c0
Robin Kuehn <kuehn.robin@gmail.com>
2016-11-02 20:10:40 -0700

    Update getCityName function

--- client/main.js
+++ client/main.js
@@ -23,7 +23,6 @@ class App extends React.Component {
     if (route.name === 'Memories') {
       return <Memories navigator={navigator} {...route.passProps}/>;
     }
-
     if (route.name === 'Sketch') {
       return <Sketch navigator={navigator} {...route.passProps}/>;
     }
